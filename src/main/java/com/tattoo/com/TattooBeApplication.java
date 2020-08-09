10e2c2e542b73fd69a47e0210f0c851b90094e91
Denis Solodkiy <d.salodki@godeltech.com>
2020-08-09 11:35:23 +0300

    Initial commit

---
+++ src/main/java/com/tattoo/com/TattooBeApplication.java
@@ -0,0 +1,12 @@
+package com.tattoo.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TattooBeApplication.class, args);
+    }
+}
